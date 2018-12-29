d0a1bf3a2e9357dfad020494f80ca5daabd54029
LisaKr <lisa.kramarenko@gmail.com>
2018-12-29 17:20:37 +0100

    Added credit to Pexels API

--- src/todo.js
+++ src/todo.js
@@ -12,11 +12,10 @@ export default function Todo() {
                 <li> Add event integration from eventbrite API (find events for that city for specific dates) 📅</li>
                 <li> Add sub-categories when choosing activities (e.g. Italian / Chinese etc or Museums / Theaters....) 📓</li>
                 <li> Add PDF export 📖</li>
-                <li> Fix small bugs 🐛 </li>
-                <li> Clean up the code  😳 </li>
                 <li> On getting API category results loop through them to adjust add/delete buttons</li>
                 <li> Combine delete and on mount functions (because the sequence of actions is the same)</li>
                 <li> Add functionality to selecting a popular city</li>
+                <li>Add opening times</li>
                 <li> Loading screen: show the element when the axios request goes to the server and hide it when the results come in from the back</li>
 
             </ul>
