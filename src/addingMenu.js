81f50b60fdc0f5e616de5f72dcb207e0f7f3e454
LisaKr <lisa.kramarenko@gmail.com>
2019-01-29 15:20:55 +0100

    added a parent div for days

--- src/addingMenu.js
+++ src/addingMenu.js
@@ -13,8 +13,7 @@ import {
     groupActivitiesForPlanPage,
     checkIfActivityAlreadyAddedToThisDay,
     showAddingError,
-    hideAddingError,
-    hideSubCategories
+    hideAddingError
 } from "./actions.js";
 
 
@@ -28,64 +27,62 @@ class AddingMenu extends React.Component {
             <div className="adding-menu">
                 <p>Which day would you like to add this activity to?</p>
                 <br/>
-                {/*for every day which is available in the arrOfDays (meaning which is still not completely full)*/}
-                {this.props.arrOfDays && this.props.arrOfDays.sort().map(
-                    day => {
-                        return(
-                            <div key={day} className="day"
-                                onClick={ () => {
-                                    {/*first checking if the selected day already has the activity added to it*/}
-                                    let prom = this.props.dispatch(checkIfActivityAlreadyAddedToThisDay(this.props.selectedActivityName, this.props.city, day));
+                <div className="days">
+                    {/*for every day which is available in the arrOfDays (meaning which is still not completely full)*/}
+                    {this.props.arrOfDays && this.props.arrOfDays.sort().map(
+                        day => {
+                            return(
+                                <div key={day} className="day"
+                                    onClick={ () => {
+                                        {/*first checking if the selected day already has the activity added to it*/}
+                                        let prom = this.props.dispatch(checkIfActivityAlreadyAddedToThisDay(this.props.selectedActivityName, this.props.city, day));
 
-                                    {/*after the checking is done and the result is set into state*/}
-                                    prom.then(()=>{
-                                        if (this.props.activityAlreadyAdded) {
-                                            this.props.dispatch(showAddingError());
-                                        } else {
-                                            {/*if the activity was not yet added*/}
-                                            const promise = addVenue(this.props.city,
-                                                this.props.selectedActivityName,
-                                                this.props.selectedActivityLocation,
-                                                this.props.category,
-                                                day);
+                                        {/*after the checking is done and the result is set into state*/}
+                                        prom.then(()=>{
+                                            if (this.props.activityAlreadyAdded) {
+                                                this.props.dispatch(showAddingError());
+                                            } else {
+                                                {/*if the activity was not yet added*/}
+                                                const promise = addVenue(this.props.city,
+                                                    this.props.selectedActivityName,
+                                                    this.props.selectedActivityLocation,
+                                                    this.props.category,
+                                                    day);
 
-                                            this.props.dispatch(promise);
+                                                this.props.dispatch(promise);
 
-                                            //after addVenue promise is fullfilled
-                                            promise.then(() => {
-                                                console.log("AFTER VENUE IS ADDED!!");
+                                                //after addVenue promise is fullfilled
+                                                promise.then(() => {
                                                 //checking for activities in the day we just added the activity to to see if it's now full
-                                                const promise1 = this.props.dispatch(checkingActivitiesInDays(day));
-                                                //and also resetting the list of activities in state to now include the newly added activity
-                                                const promise2 = this.props.dispatch(putActivitiesInState(this.props.city));
-                                                //after it is done we group activities by days for the plan page
-                                                Promise.all([promise1, promise2]).then(()=> {
-                                                    console.log("PROMISE OF PUTTING ACTIVITIES INTO STATE FULFILLED!!");
-                                                    this.props.dispatch(groupActivitiesForPlanPage());
-                                                    console.log("AND NOW GROUPING THINGS!!");
-
+                                                    const promise1 = this.props.dispatch(checkingActivitiesInDays(day));
+                                                    //and also resetting the list of activities in state to now include the newly added activity
+                                                    const promise2 = this.props.dispatch(putActivitiesInState(this.props.city));
+                                                    //after it is done we group activities by days for the plan page
+                                                    Promise.all([promise1, promise2]).then(()=> {
+                                                        this.props.dispatch(groupActivitiesForPlanPage());
+                                                    });
                                                 });
-                                            });
-                                            //after the venue is added and activities are put in state we are going through all the displayed
-                                            //category results and checking whether any of them was the one we just added
-                                            //if it's true we are changing it's deletable property so that the add/delete button toggling happens
-                                            for (let i =0; i<this.props.categoryResults.length; i++) {
-                                                if (this.props.categoryResults[i].name == this.props.selectedActivityName) {
-                                                    this.props.dispatch(setDeletablePropertyToTrue(this.props.categoryResults[i].name));
+                                                //after the venue is added and activities are put in state we are going through all the displayed
+                                                //category results and checking whether any of them was the one we just added
+                                                //if it's true we are changing it's deletable property so that the add/delete button toggling happens
+                                                for (let i =0; i<this.props.categoryResults.length; i++) {
+                                                    if (this.props.categoryResults[i].name == this.props.selectedActivityName) {
+                                                        this.props.dispatch(setDeletablePropertyToTrue(this.props.categoryResults[i].name));
+                                                    }
                                                 }
+                                                //showing the pop-up about adding the activity and hiding the menu with the day selection
+                                                this.props.dispatch(hideAddingMenu());
+                                                // this.props.dispatch(hideSubCategories());
+                                                this.props.dispatch(successfullyAdded(this.props.selectedActivityName));
                                             }
-                                            //showing the pop-up about adding the activity and hiding the menu with the day selection
-                                            this.props.dispatch(hideAddingMenu());
-                                            // this.props.dispatch(hideSubCategories());
-                                            this.props.dispatch(successfullyAdded(this.props.selectedActivityName));
-                                        }
-                                    });
-                                }}>
-                                 day {day}
-                            </div>
-                        );
-                    }
-                )}
+                                        });
+                                    }}>
+                                    <p>Day {day}</p>
+                                </div>
+                            );
+                        }
+                    )}
+                </div>
                 <br/>
                 <button onClick={()=> {
                     this.props.dispatch(hideAddingMenu());
